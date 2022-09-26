be5f52538e4524dfc238b8ff63912d29027a11af
me <me@example.org>
2022-09-26 21:50:34 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+BigSoftware - Demonstrating continuous integration with Rust
+
+This project mimmicks a big software project so that concepts behind continuous
+integration can be demonstrated on a smaller scale.
+
+This project is part of the Rust Berlin Online Meetup.
---
+++ src/lib.rs
@@ -0,0 +1,153 @@
+//! This is supposed to be a very big TODO list management project.
+//!
+//! A lot of people are supposed to work together on building this.
+//!
+//! Let's break the docstring
+//!
+//! (foo)[bar.html]
+
+#![deny(missing_docs)]
+
+use std::cmp::Ordering;
+
+/// A task that needs to be done
+#[derive(PartialEq, Eq)]
+pub struct Todo {
+    // TODO add assigned user
+    title: String,
+    description: String,
+    urgency: Urgency,
+}
+
+impl Todo {
+    /// Create a new TODO item
+    pub fn new(title: &str, description: &str, urgency: Urgency) -> Self {
+        Self {
+            title: title.to_string(),
+            description: description.to_string(),
+            urgency,
+        }
+    }
+}
+
+/// Describe  how urgently something needs to be done
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Urgency {
+    // TODO more variants, TODO maybe different ordering?
+    /// No deadline
+    NonExisting,
+    /// Should be done evenually
+    Low,
+    /// Regular priority
+    Medium,
+    /// Get it done ASAP
+    High,
+    /// Drop everything and do this now!
+    ExtremlyUrgent,
+}
+
+impl Ord for Urgency {
+    fn cmp(&self, other: &Self) -> Ordering {
+        fn value(urgency: &Urgency) -> usize {
+            // TODO technically not needed
+            match urgency {
+                Urgency::NonExisting => 0,
+                Urgency::Low => 1,
+                Urgency::Medium => 2,
+                Urgency::High => 3,
+                Urgency::ExtremlyUrgent => 4,
+            }
+        }
+        value(self).cmp(&value(other))
+    }
+}
+
+impl PartialOrd for Urgency {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Todo {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.urgency.cmp(&other.urgency)
+    }
+}
+
+// TODO Demonstrate: derive this trait and unittest the result
+impl PartialOrd for Todo {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+/// A list of TODO items
+pub struct TodoList {
+    items: Vec<Todo>,
+}
+
+impl TodoList {
+    /// Create a new list
+    pub fn new() -> Self {
+        Self { items: vec![] }
+    }
+
+    /// Add a task to the list
+    pub fn add_task(&mut self, title: &str, description: &str, urgency: Urgency) {
+        self.items.push(Todo::new(title, description, urgency));
+        self.items.sort_unstable(); // TODO find insertion position instead
+    }
+
+    /// Get the most urgent task right now
+    pub fn get_task(&mut self) -> Option<Todo> {
+        self.items.pop()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_sorting() {
+        let mut tasks = vec![
+            Todo::new("Prepare talk", "Talk about CI", Urgency::ExtremlyUrgent),
+            Todo::new(
+                "Learn Fortran",
+                "Because Dinosaurs are cool",
+                Urgency::NonExisting,
+            ),
+            Todo::new("Buy groceries", "Need to eat", Urgency::High),
+            Todo::new("Do the dishes", "Almost out of plates", Urgency::Medium),
+        ];
+        tasks.sort_unstable();
+        assert_eq!(tasks[3].title, "Prepare talk");
+        assert_eq!(tasks[2].description, "Need to eat");
+        assert_eq!(tasks[1].urgency, Urgency::Medium);
+        assert_eq!(tasks[0].description, "Because Dinosaurs are cool");
+    }
+
+    #[test]
+    fn test_list() {
+        let mut list = TodoList::new();
+        list.add_task(
+            "Write a TODO list",
+            "good examples are hard to find",
+            Urgency::Medium,
+        );
+        list.add_task(
+            "Teach rustaceans to do CI",
+            "So that open source will be even better",
+            Urgency::High,
+        );
+        list.add_task(
+            "Find more silly examples",
+            "Because short lists are boring",
+            Urgency::Low,
+        );
+        assert_eq!(list.get_task().unwrap().title, "Teach rustaceans to do CI");
+        assert_eq!(list.get_task().unwrap().title, "Write a TODO list");
+        assert_eq!(list.get_task().unwrap().title, "Find more silly examples");
+        assert!(list.get_task().is_none());
+    }
+}
